81d4958f70c0d9b2ec55a98bece792785a723a7f
Moosa Khalid <moosa.khalid@workday.com>
2022-11-16 20:30:14 +0000

    Added s3 bucket module again - fixed

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/moosakhalidworkday/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "moosakhalid2"
 }
